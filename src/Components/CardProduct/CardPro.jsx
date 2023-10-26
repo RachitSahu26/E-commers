f3ebdd00581bc14351be7ae2c7ef424eb955140c
rachit <curiousrachit.26@gmail.com>
2023-10-26 21:03:00 +0530

    adding new product page

---
+++ src/Components/CardProduct/CardPro.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import "./CardPro.css";
+import shoe4 from "./CardDetailImg/shoe4.jpg"
+export default function CardPro() {
+    return (
+        <div className=' CardProduct'>
+
+            {/* .............right product  image.............. */}
+
+
+            <div className="left">
+
+                {/* ...........img box........ */}
+                <div className="img-box">
+                    <img src={shoe4}></img>
+                </div>
+
+
+
+            </div>
+
+            {/* .............right product detail.............. */}
+            <div className="right">
+               
+               
+               
+                <div className="shoe-detail">
+                    <h3>U.S. POLO ASSN.</h3>
+                    <p>Lebron 3.0 Blu Mens Textured Off White Sneakers</p>
+                    <p>Based on 25 ratings</p>
+                    <span>₹2,36545% Off</span>
+
+                </div>
+                
+                
+                
+                
+                
+                <hr></hr>
+
+
+
+
+
+                {/* ................................size box .......................... */}
+                <div className="size-box">
+
+                    <h3>Select Size</h3>
+                    <button >S</button>
+                    <button>M</button>
+                    <button>L</button>
+                    <button>Xl</button>
+
+                </div>
+
+
+
+
+
+
+                {/* .................................add to card and wishlist ............... */}
+
+<div className="addCard-addWish">
+<button>Add To Card</button>
+<button>Add To Bag</button>
+
+
+</div>
+
+
+
+            </div>
+
+
+
+        </div>
+    )
+}
--- src/Pages/CardDetail/CardDetail.jsx
+++ src/Pages/CardDetail/CardDetail.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
+import CardPro from '../../Components/CardProduct/CardPro'
 
 function CardDetail() {
   return (
     <div>
       <NavBar/>
-
+<CardPro/>
     </div>
   )
 }
