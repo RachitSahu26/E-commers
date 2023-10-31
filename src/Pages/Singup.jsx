5526b9d59a493c56c825df4f1ea8485e1652343e
rachit <curiousrachit.26@gmail.com>
2023-10-31 20:50:24 +0530

    created a signup page

---
+++ src/Pages/Singup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignupForm from '../Components/SignupForm/SignupForm';
+
+function Singup() {
+
+  return (
+<>
+<SignupForm/>
+</>
+  );
+}
+
+export default  Singup;
