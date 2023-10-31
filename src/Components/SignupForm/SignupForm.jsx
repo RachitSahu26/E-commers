0e921d7cb67a5811c06c9fcead1c68d6325b0296
rachit <curiousrachit.26@gmail.com>
2023-10-31 20:54:52 +0530

    created signup page

---
+++ src/Components/SignupForm/SignupForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import "../SignupForm/SignupForm.scss";
+function SignupForm() {
+  // Define state variables for name, email and password
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validate input fields and send data to server
+    console.log(name, email, password);
+  };
+
+  return (
+    <div className="signup-form">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
