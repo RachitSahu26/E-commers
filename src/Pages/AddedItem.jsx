8cac9ecf54b3b57233a81f1fc55eea13eff10df6
rachit <curiousrachit.26@gmail.com>
2023-10-31 21:02:06 +0530

    cart section. i have created

---
+++ src/Pages/AddedItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemAdded from '../Components/ItemAdded/ItemAdded'
+
+function AddedItem() {
+  return (
+    <div>
+      <ItemAdded/>
+    </div>
+  )
+}
+
+export default AddedItem
