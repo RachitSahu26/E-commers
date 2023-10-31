3f2cce272aa29d0823abf45f832aa86482747cf6
rachit <curiousrachit.26@gmail.com>
2023-10-31 21:01:25 +0530

    Created Popularity Section

---
+++ src/Components/Popularity/Popularity.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Popularity.scss";
+import Item from '../Item/Item';
+import  Collection from"../assets/new_collections";
+function Popularity() {
+  return (
+    <div className='Popularity'>
+      <h1>POPULARITY</h1>
+
+<div className="product-area">
+{
+Collection.map((item,i)=>{
+    return (
+        <Item
+         key={i} 
+         name={item.name} 
+         image={item.image} 
+          new_price={item.new_price} 
+          old_price={item.old_price} 
+            />
+    )
+})
+
+}
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default Popularity
