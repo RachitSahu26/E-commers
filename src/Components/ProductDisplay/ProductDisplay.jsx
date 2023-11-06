76b9af3e6748ef22e76279e192636e85dfcffb9e
rachit <curiousrachit.26@gmail.com>
2023-11-06 11:36:08 +0530

    Created product display page

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,20 +1,70 @@
 import React from 'react'
-import "./ProductDis.css";
+import "./ProductDis.scss";
 function ProductDisplay(props) {
-const { product }=props;
+    const { product } = props;
     return (
 
 
         <div>
             <div className="productDis">
-                {/* ..........................left display part ....... */}
 
+
+
+
+
+                {/* ..........................left display part ....... */}
                 <div className="leftDis">
-                    <img src={product.image } />
-                dfdsfsdfd
+
+                    <div className="collection-img">
+                        <img src={product.image} />
+                        <img src={product.image} />
+                        <img src={product.image} />
+                        <img src={product.image} />
+
+
+                    </div>
+
+
+
+                    <div className="mainImg">
+                        <img src={product.image} />
+
+
+                    </div>
+
                 </div>
+
+
+
+
+
                 {/* ..........................right display part ....... */}
                 <div className="rightDis">
+
+{/* .................detail.............. */}
+                    <div className="detail">
+
+                        <h1> {product.name} </h1>
+                        <p>${product.old_price} , MRP ₹2999(69% OFF)</p>
+                        <p>${product.new_price}</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, mollitia.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, adipisci?
+                        </p>
+                    </div>
+{/* .................size select.............. */}
+
+                    <div className="size-select">
+                        <h3>Select Size</h3>
+                        <button >S</button>
+                        <button>M</button>
+                        <button>L</button>
+                        <button>Xl</button>
+
+                    </div>
+{/* .................add to card.............. */}
+<button>Add to Card</button>
+
+
 
                 </div>
 
